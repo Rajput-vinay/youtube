0c71c420cc7d57306a747fc2e5780b0cfc9aaa86
Vinay Rajput <105452265+Rajput-vinay@users.noreply.github.com>
2023-12-20 01:19:19 +0530

    Update constant.js
    new

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,6 @@
 const GOOGLE_API_KEY ="AIzaSyB2_Ppru607SZnvJSIVDn6I-is9hfx1rXg"
 export const YOUTUBE_VIDEOS_API ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY ;
 
-export const YOUTUBE_SEARCH_API ="https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-
+export const YOUTUBE_SEARCH_API ='https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
 export const LIVE_CHAT_COUNT = 25;
 
